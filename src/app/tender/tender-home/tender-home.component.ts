6265cd80e1d2eceaeb623dd0c40186a13ee481d7
drasko1959 <34159869+drasko1959@users.noreply.github.com>
2020-11-26 00:12:25 +0100

    material tab

--- src/app/tender/tender-home/tender-home.component.ts
+++ src/app/tender/tender-home/tender-home.component.ts
@@ -4,6 +4,7 @@ import {Ponude} from '../model/ponude.model';
 import {MatSort} from '@angular/material/sort';
 import {MatPaginator} from '@angular/material/paginator';
 import {TenderService} from '../tender.service';
+import {Bodovanje} from '../model/bodovanje.model';
 
 @Component({
   selector: 'app-tender-home',
@@ -19,7 +20,8 @@ export class TenderHomeComponent implements OnInit, AfterViewInit {
   public displayedColumns = ['id', 'partija', 'atc', 'nazivProizvoda', 'zasticeniNaziv', 'proizvodjac',
     'jedinicaMjere', 'trazenaKolicina', 'ponudjanaKolicina', 'procijenjenaJedinicnaCijena', 'ponudjenaJedinicnaCijena',
     'procijenjenaUkupnaCijena', 'ponudjenaUkupnaCijena', 'rokIsporuke' , 'ponudjac', 'brojTendera', 'actions'];
-  public dataSource = new MatTableDataSource<Ponude>();
+  public dataSource1 = new MatTableDataSource<Ponude>();
+  public dataSource2 = new MatTableDataSource<Bodovanje>();
 
   @ViewChild(MatSort) sort: MatSort;
   @ViewChild(MatPaginator) paginator: MatPaginator;
@@ -29,19 +31,26 @@ export class TenderHomeComponent implements OnInit, AfterViewInit {
   // tslint:disable-next-line:typedef
   ngOnInit() {
     this.getAllOwners();
+    this.getAllBodovanje();
   }
-
+  // tslint:disable-next-line:typedef
+  private getAllBodovanje()  {
+    this.tenderService.getBodovanje()
+      .subscribe(res => {
+        this.dataSource2.data = res as Bodovanje[];
+      });
+  }
   // tslint:disable-next-line:typedef
   public getAllOwners()  {
     this.tenderService.getData()
       .subscribe(res => {
-        this.dataSource.data = res as Ponude[];
+        this.dataSource1.data = res as Ponude[];
       });
   }
 
   ngAfterViewInit(): void {
-    this.dataSource.sort = this.sort;
-    this.dataSource.paginator = this.paginator;
+    this.dataSource1.sort = this.sort;
+    this.dataSource1.paginator = this.paginator;
   }
 
   public customSort = (event) => {
@@ -49,7 +58,7 @@ export class TenderHomeComponent implements OnInit, AfterViewInit {
   }
 
   public doFilter = (value: string) => {
-    this.dataSource.filter = value.trim().toLocaleLowerCase();
+    this.dataSource1.filter = value.trim().toLocaleLowerCase();
   }
 
 }
