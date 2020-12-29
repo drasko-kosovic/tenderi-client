5093f4da2ae3a751b3b124bafb4f740d8f9b43e1
drasko1959 <drasko.kosovic@gmail.com>
2020-12-29 10:37:59 +0100

    init

--- src/app/tender/tender.service.ts
+++ src/app/tender/tender.service.ts
@@ -13,36 +13,36 @@ export class TenderService {
 
   
   // readonly API_URL_PREKO_PROCIJENJE = 'http://localhost:8080/api/ponude/preko_procijenjene/';
-  private readonly API_URL_DELETE_SELECTED = 'http://localhost:8080/api/ponude/delete/selected';
-  private readonly API_URL = 'http://localhost:8080/api/ponude';
-  private readonly API_URL_DELETE = 'http://localhost:8080/api/ponude/delete/';
-  private readonly API_URL_UPDATE_SELECTED = 'http://localhost:8080/api/ponude/update/selected/';
-  private readonly API_URL_BODOVANJE = 'http://localhost:8080/api/ponude/bodovanje/';
-  private readonly API_URL_PRVORANGIRANI = 'http://localhost:8080/api/ponude/prvorangirani/';
-  readonly API_URL_ADD = 'http://localhost:8080/api/ponude/add';
-  readonly API_URL_UPDATE = 'http://localhost:8080/api/ponude/update';
-  readonly API_URL_FIND_BY_TENDER = 'http://localhost:8080/api/ponude/tender/';
-  readonly PREKO_PROCIJENJENE_URL_FIND_BY_TENDER = 'http://localhost:8080/api/ponude/tender/preko_procijenjene/';
-  readonly API_URL_HVALE_PARTIJE_BY_TENDER = 'http://localhost:8080/api/ponude/hvali/';
-  readonly BODOVANJE_URL_FIND_BY_TENDER = 'http://localhost:8080/api/ponude/bodovanje/';
-  readonly PRVORANIRANI_URL_FIND_BY_TENDER = 'http://localhost:8080/api/ponude/tender/prvorangirani/';
+  // private readonly API_URL_DELETE_SELECTED = 'http://localhost:8080/api/ponude/delete/selected';
+  // private readonly API_URL = 'http://localhost:8080/api/ponude';
+  // private readonly API_URL_DELETE = 'http://localhost:8080/api/ponude/delete/';
+  // private readonly API_URL_UPDATE_SELECTED = 'http://localhost:8080/api/ponude/update/selected/';
+  // private readonly API_URL_BODOVANJE = 'http://localhost:8080/api/ponude/bodovanje/';
+  // private readonly API_URL_PRVORANGIRANI = 'http://localhost:8080/api/ponude/prvorangirani/';
+  // readonly API_URL_ADD = 'http://localhost:8080/api/ponude/add';
+  // readonly API_URL_UPDATE = 'http://localhost:8080/api/ponude/update';
+  // readonly API_URL_FIND_BY_TENDER = 'http://localhost:8080/api/ponude/tender/';
+  // readonly PREKO_PROCIJENJENE_URL_FIND_BY_TENDER = 'http://localhost:8080/api/ponude/tender/preko_procijenjene/';
+  // readonly API_URL_HVALE_PARTIJE_BY_TENDER = 'http://localhost:8080/api/ponude/hvali/';
+  // readonly BODOVANJE_URL_FIND_BY_TENDER = 'http://localhost:8080/api/ponude/bodovanje/';
+  // readonly PRVORANIRANI_URL_FIND_BY_TENDER = 'http://localhost:8080/api/ponude/tender/prvorangirani/';
 
 
   
-  // readonly API_URL_PREKO_PROCIJENJE = 'https://tenderi-montefarm.herokuapp.com/api/ponude/preko_procijenjene/';
-  // private readonly API_URL_DELETE_SELECTED = 'https://tenderi-montefarm.herokuapp.com/api/ponude/delete/selected';
-  // private readonly API_URL = 'https://tenderi-montefarm.herokuapp.com/api/ponude';
-  // private readonly API_URL_DELETE = 'https://tenderi-montefarm.herokuapp.com/api/ponude/delete/';
-  // private readonly API_URL_UPDATE_SELECTED = 'https://tenderi-montefarm.herokuapp.com/api/ponude/update/selected/';
-  // private readonly API_URL_BODOVANJE = 'https://tenderi-montefarm.herokuapp.com/api/ponude/bodovanje/';
-  // private readonly API_URL_PRVORANGIRANI = 'https://tenderi-montefarm.herokuapp.com/api/ponude/prvorangirani/';
-  // readonly API_URL_ADD = 'https://tenderi-montefarm.herokuapp.com/api/ponude/add';
-  // readonly API_URL_UPDATE = 'https://tenderi-montefarm.herokuapp.com/api/ponude/update';
-  // readonly API_URL_FIND_BY_TENDER = 'https://tenderi-montefarm.herokuapp.com/api/ponude/tender/';
-  // readonly PREKO_PROCIJENJENE_URL_FIND_BY_TENDER = 'https://tenderi-montefarm.herokuapp.com/api/ponude/tender/preko_procijenjene/';
-  // readonly API_URL_HVALE_PARTIJE_BY_TENDER = 'https://tenderi-montefarm.herokuapp.com/api/ponude/hvali/';
-  // readonly BODOVANJE_URL_FIND_BY_TENDER = 'https://tenderi-montefarm.herokuapp.com/api/ponude/bodovanje/';
-  // readonly PRVORANIRANI_URL_FIND_BY_TENDER = 'https://tenderi-montefarm.herokuapp.com/api/ponude/tender/prvorangirani/';
+  readonly API_URL_PREKO_PROCIJENJE = 'https://tenderi-montefarm.herokuapp.com/api/ponude/preko_procijenjene/';
+  private readonly API_URL_DELETE_SELECTED = 'https://tenderi-montefarm.herokuapp.com/api/ponude/delete/selected';
+  private readonly API_URL = 'https://tenderi-montefarm.herokuapp.com/api/ponude';
+  private readonly API_URL_DELETE = 'https://tenderi-montefarm.herokuapp.com/api/ponude/delete/';
+  private readonly API_URL_UPDATE_SELECTED = 'https://tenderi-montefarm.herokuapp.com/api/ponude/update/selected/';
+  private readonly API_URL_BODOVANJE = 'https://tenderi-montefarm.herokuapp.com/api/ponude/bodovanje/';
+  private readonly API_URL_PRVORANGIRANI = 'https://tenderi-montefarm.herokuapp.com/api/ponude/prvorangirani/';
+  readonly API_URL_ADD = 'https://tenderi-montefarm.herokuapp.com/api/ponude/add';
+  readonly API_URL_UPDATE = 'https://tenderi-montefarm.herokuapp.com/api/ponude/update';
+  readonly API_URL_FIND_BY_TENDER = 'https://tenderi-montefarm.herokuapp.com/api/ponude/tender/';
+  readonly PREKO_PROCIJENJENE_URL_FIND_BY_TENDER = 'https://tenderi-montefarm.herokuapp.com/api/ponude/tender/preko_procijenjene/';
+  readonly API_URL_HVALE_PARTIJE_BY_TENDER = 'https://tenderi-montefarm.herokuapp.com/api/ponude/hvali/';
+  readonly BODOVANJE_URL_FIND_BY_TENDER = 'https://tenderi-montefarm.herokuapp.com/api/ponude/bodovanje/';
+  readonly PRVORANIRANI_URL_FIND_BY_TENDER = 'https://tenderi-montefarm.herokuapp.com/api/ponude/tender/prvorangirani/';
 
 
 
