8978bd0757b3c6e093d3e7c255a75c1f593dcdc4
drasko1959 <drasko.kosovic@gmail.com>
2021-01-10 21:48:03 +0100

    init

--- src/app/auth/users/users.component.ts
+++ src/app/auth/users/users.component.ts
@@ -3,7 +3,6 @@ import { MatTableDataSource } from '@angular/material/table';
 import { Router } from '@angular/router';
 import { Users } from 'src/app/tender/model/users.model';
 import { UserService } from '../login/_services/user.service';
-
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
@@ -11,15 +10,11 @@ import { UserService } from '../login/_services/user.service';
 })
 export class UsersComponent implements OnInit {
   public displayedColumns = ['id', 'username', 'email'];
-
   public dataSource = new MatTableDataSource<Users>();
-
   constructor(private userService: UserService, private router: Router) {}
-
   ngOnInit() {
     this.getAllUsers();
   }
-
   public getAllUsers() {
     this.userService.getUsers().subscribe((res) => {
       this.dataSource.data = res as Users[];
